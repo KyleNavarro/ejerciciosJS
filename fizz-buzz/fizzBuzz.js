d752e6eaa4085f37d3e1bbb9b97dbac4f17c4a6a
kyle <kyle.y.navarro.j@gmail.com>
2025-07-29 06:48:15 -0500

    enunciado de fizzbuzz

--- fizz-buzz/fizzBuzz.js
+++ fizz-buzz/fizzBuzz.js
@@ -1,14 +1,22 @@
+/*
+ * Escribe un programa que muestre por consola (con un print) los
+ * números de 1 a 100 (ambos incluidos y con un salto de línea entre
+ * cada impresión), sustituyendo los siguientes:
+ * - Múltiplos de 3 por la palabra "fizz".
+ * - Múltiplos de 5 por la palabra "buzz".
+ * - Múltiplos de 3 y de 5 a la vez por la palabra "fizzbuzz".
+ */
+
 function fizzBuzz(numeros){
-    for (let i = 1; i <= numeros; i++){
-        if (i % 3 === 0 && i % 5 === 0){
-            console.log('fizzBuuzz');
+    for(i = 0; i <= numeros; i++){}
+        if (i % 3 === 0  && i % 5 === 0){
+            console.log('FizzBuzz')
         }else if(i % 3 === 0){
-            console.log('Fizz');
-        }else if(i % 5 === 0){
-            console.log('Buzz');
+            console.log('Fizz')
+        }else if(i % 5 === 0 ){
+            console.log('Buzz')
         }else{
-            console.log(i);
+            console.log(i)
         }
-    }
-}
-fizzBuzz(100)
+}   
+fizzBuzz(100);
