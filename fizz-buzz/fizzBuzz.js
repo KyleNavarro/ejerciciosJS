f1bae3f7134422f8320dd2c34424d2c9560fef01
kyle <kyle.y.navarro.j@gmail.com>
2025-07-30 04:56:24 -0500

    corregimos detalles

--- fizz-buzz/fizzBuzz.js
+++ fizz-buzz/fizzBuzz.js
@@ -8,15 +8,15 @@
  */
 
 function fizzBuzz(numeros){
-    for(i = 0; i <= numeros; i++){}
+    for(let i = 0; i <= numeros; i++){
         if (i % 3 === 0  && i % 5 === 0){
             console.log('FizzBuzz')
         }else if(i % 3 === 0){
             console.log('Fizz')
         }else if(i % 5 === 0 ){
             console.log('Buzz')
-        }else{
-            console.log(i)
+        }else console.log(i)
         }
+     
 }   
 fizzBuzz(100);
