3435aeb3f78091fc5ed9477d862dad3831733053
kyle <kyle.y.navarro.j@gmail.com>
2025-08-05 06:42:32 -0500

    empezamos a estudiar objetos

--- miniEcommerce/index.js
+++ miniEcommerce/index.js
@@ -1,1 +1,31 @@
-// terminamos en 5:07
+const productos = [
+    ['tarta de everdura', 200],
+    ['milanesa', 500],
+    ['palanga', 40],
+    ['alfajor jorgito', 100],
+    ['mouse read dragon', 340],
+    ['gaseosa', 5000],
+    ['mayonesa', 40]
+]
+//podríamos iterar con un ciclo for.
+function crearMenuProductos(){
+    for(let i = 0; i < productos.length; i++){
+        for(let j = 0; j < productos[i].length ; j++){
+            document.writeln(productos[i][j]);
+        }       
+        document.writeln('<br>')
+    }  
+}
+
+
+
+function crearMenuProductosForEach(){
+    productos.forEach(producto =>{
+        producto.forEach(dato =>{
+            document.writeln(dato)
+        })
+        document.writeln('<br>')
+    })
+}
+
+//el for primero pasa por cada elimento y no pasa al siguiente hasta iterar todos los elementos de la matriz   
---
+++ objetos-clases/object.js
@@ -0,0 +1,19 @@
+const name = 'German rodriguez';
+
+
+const streamer = {
+    name,
+    viewers: 49,
+    followers: 5300,
+    saludar: function(){
+        console.log('hola')
+    }
+} 
+
+
+
+streamer.userName = 'padawansTariner'
+
+console.log(streamer);
+streamer.saludar();
+
